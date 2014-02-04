1272d681d738c183af950a04460f3f81b3235cd9
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-04 11:55:55 +0200

    Second draft of api component

--- src/api.js
+++ src/api.js
@@ -2,20 +2,20 @@ vumi_ureport.api = function() {
     var vumigo = require('vumigo_v02');
     var utils = vumigo.utils;
     var Extendable = utils.Extendable;
+
     var JsonApi = vumigo.http_api.JsonApi;
+    var HttpResponseError = vumigo.http_api.HttpResponseError;
 
     var UReportApi = Extendable.extend(function(self, im, base_url, opts) {
         self.http = JsonApi.call(self, im, opts);
         self.base_url = base_url;
 
         self.url = function(paths) {
-            return [self.base_url]
-                .concat(paths)
-                .join('/');
+            return [self.base_url, paths].join('/');
         };
 
         self.ureporters = function(backend, user_addr) {
-            return new UReportApi();
+            return new UReportersApi(self, backend, user_addr);
         };
     });
 
@@ -24,26 +24,29 @@ vumi_ureport.api = function() {
         self.backend = backend;
         self.user_addr = user_addr;
 
-        self.url = function(paths) {
-            return self.api.url([
-                'ureporters',
-                self.backend,
-                self.user_addr
-            ].concat(paths));
+        self.url = function(path) {
+            return self.api.url(['ureporters', path].join('/'));
         };
 
         self.get = function() {
-            return self.api.http.get(self.url());
+            return self
+                .api.http.get(self.url())
+                .get('user')
+                .catch(catch_code(404, null));
         };
 
         self.polls = {};
 
         self.polls.current = function() {
-            return self.api.http.get(self.url(['polls', 'current']));
+            return self
+                .api.http.get(self.url('polls/current'))
+                .get('poll');
         };
 
         self.polls.topics = function() {
-            return self.api.http.get(self.url(['polls', 'topics']));
+            return self
+                .api.http.get(self.url('polls/topics'))
+                .get('poll_topics');
         };
 
         self.poll = function(id) {
@@ -52,9 +55,11 @@ vumi_ureport.api = function() {
 
         self.reports = {};
         self.reports.submit = function(report) {
-            return self.api.http.post(self.url(['reports/']), {
-                data: {report: report}
-            });
+            return self
+                .api.http.post(self.url('reports/'), {
+                    data: {report: report}
+                })
+                .get('result');
         };
     });
 
@@ -67,21 +72,38 @@ vumi_ureport.api = function() {
             return self.ureporter.url([
                 'poll',
                 self.id
-            ].concat(paths));
+            ].join('/'));
         };
 
         self.responses = {};
 
         self.responses.submit = function(response) {
-            return self.api.http.post(self.url(['responses/']), {
-                data: {response: response}
-            });
+            return self
+                .api.http.post(self.url('responses/'), {
+                    data: {response: response}
+                })
+                .get('result');
         };
 
         self.summary = function() {
-            return self.api.http.get(self.url(['summary']));
+            return self
+                .api.http.get(self.url('summary'))
+                .get('poll_result');
         };
     });
+
+    function catch_code(code, result) {
+        return function(e) {
+            var match = e instanceof HttpResponseError
+                     && e.reponse.code === code;
+
+            if (!match) {
+                throw e;
+            }
+
+            return utils.maybe_call(result);
+        };
+    }
 
     return {
         UReportApi: UReportApi,
