14c74b6bb64375bb7293d379ec97dcfb1870d810
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-06 10:14:01 +0200

    Add a stub-based DummyUReportApi

--- src/dummy.js
+++ src/dummy.js
@@ -1,8 +1,36 @@
 var Q = require('q');
 var sinon = require('sinon');
 
+var vumigo = require('vumigo_v02');
+var utils = vumigo.utils;
+var Extendable = utils.Extendable;
+
 
 vumi_ureport.dummy = function() {
+    var DummyUReportApi = Extendable.extend(function(self) {
+        self.ureporters = utils.functor(new DummyUReportersApi());
+    });
+
+    var DummyUReportersApi = Extendable.extend(function(self) {
+        self.get = stub('ureporters.get');
+
+        self.polls = {};
+        self.polls.current = stub('ureporters.polls.current');
+        self.polls.topics = stub('ureporters.polls.topics');
+
+        self.reports = {};
+        self.reports.submit = stub('ureporters.reports.submit');
+
+        self.poll = utils.functor(new DummyPollApi());
+    });
+
+    var DummyPollApi = Extendable.extend(function(self) {
+        self.responses = {};
+        self.responses.submit = stub('ureporters.poll.responses.submit');
+
+        self.summary = stub('ureporters.poll.summary');
+    });
+
     function stub(name) {
         var s = sinon.stub();
         var invoke = s.invoke;
@@ -15,10 +43,14 @@ vumi_ureport.dummy = function() {
             throw new Error("No return value provided for '" + name + "'");
         }));
 
+        s._name_ = name;
         return s;
     }
 
     return {
+        DummyUReportApi: DummyUReportApi,
+        DummyUReportersApi: DummyUReportersApi,
+        DummyPollApi: DummyPollApi,
         stub: stub
     };
 }();
--- test/dummy.test.js
+++ test/dummy.test.js
@@ -1,7 +1,9 @@
 var Q = require('q');
+var sinon = require('sinon');
 var assert = require('assert');
 
 var dummy = vumi_ureport.dummy;
+var DummyUReportApi = vumi_ureport.dummy.DummyUReportApi;
 
 
 describe("dummy", function() {
@@ -21,6 +23,72 @@ describe("dummy", function() {
                 assert.equal(
                     e.message,
                     "No return value provided for 'some.stub'");
+            });
+        });
+
+        it("should attach the stub's name as an internal property",
+        function() {
+            var s = dummy.stub('some.stub');
+            assert.equal(s._name_, 'some.stub');
+        });
+    });
+
+    describe("DummyUReportApi", function() {
+        var ureport;
+
+        function isStub(obj, name) {
+            return obj._name_ === name && 'returns' in obj;
+        }
+
+        beforeEach(function() {
+            ureport = new DummyUReportApi();
+        });
+
+        describe(".ureporters.get", function() {
+            it("should be a stub", function() {
+                assert(isStub(
+                    ureport.ureporters().get,
+                    'ureporters.get'));
+            });
+        });
+
+        describe(".ureporters.polls.current", function() {
+            it("should be a stub", function() {
+                assert(isStub(
+                    ureport.ureporters().polls.current,
+                    'ureporters.polls.current'));
+            });
+        });
+
+        describe(".ureporters.polls.topics", function() {
+            it("should be a stub", function() {
+                assert(isStub(
+                    ureport.ureporters().polls.topics,
+                    'ureporters.polls.topics'));
+            });
+        });
+
+        describe(".ureporters.reports.submit", function() {
+            it("should be a stub", function() {
+                assert(isStub(
+                    ureport.ureporters().reports.submit,
+                    'ureporters.reports.submit'));
+            });
+        });
+
+        describe(".ureporters.poll.responses.submit", function() {
+            it("should be a stub", function() {
+                assert(isStub(
+                    ureport.ureporters().poll().responses.submit,
+                    'ureporters.poll.responses.submit'));
+            });
+        });
+
+        describe(".ureporters.poll.summary", function() {
+            it("should be a stub", function() {
+                assert(isStub(
+                    ureport.ureporters().poll().summary,
+                    'ureporters.poll.summary'));
             });
         });
     });
